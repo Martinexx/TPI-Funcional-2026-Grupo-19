c9dad0914779796a97ccfb871d163dd3da7b90b0
valentino-jara-rey <valentinojararey@gmail.com>
2026-06-13 20:06:09 -0300

    Update solucion.scala

--- comparativa/solucion.scala
+++ comparativa/solucion.scala
@@ -1,1 +1,18 @@
-
+/*
+Funcion: timer
+Naturaleza: pura
+Estrategia: utilizacion de condicionales para determinar el rango
+Impacto: no destrutiva
+*/
+def timer (segundos:Int): String = {
+segundos match {
+	case segundos if ((segundos % 216) <= 90) => "rojo"
+	case segundos if ((segundos % 216) <= 210) => "verde"
+	case _ => "amarillo"
+}
+}
+/*
+casos de prueba:
+caso normal: 220 -  "rojo"
+caso error: cualquier parametro que no sea numero entero
+*/
