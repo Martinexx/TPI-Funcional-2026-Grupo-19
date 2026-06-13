c62dd8a9d6f195030bf9fcd26800426060d51f73
valentino-jara-rey <valentinojararey@gmail.com>
2026-06-13 20:51:52 -0300

    Update solucion.scala

--- comparativa/solucion.scala
+++ comparativa/solucion.scala
@@ -16,3 +16,27 @@ casos de prueba:
 caso normal: 220 -  "rojo"
 caso error: cualquier parametro que no sea numero entero
 */
+
+
+/*
+Funcion: timer_inter
+Naturaleza: pura
+Estrategia: utilizacion de condicionales para determinar el rango
+Impacto: no destrutiva
+*/
+def timer_inter (segundos:Int): String = {
+segundos match {
+	case segundos if ((segundos % 216) <= 87) => "rojo"
+	case segundos if ((segundos % 216) <= 90) => "rojo-intermitente"
+	case segundos if ((segundos % 216) <= 207) => "verde"
+	case segundos if ((segundos % 216) <= 210) => "verde-intermitente"
+	case segundos if ((segundos % 216) <= 213) => "amarillo"
+	case _ => "amarillo-intermitente"
+}
+}
+/*
+casos de prueba:
+caso normal: 304 - "rojo-intermitente" 
+caso error: cualquier parametro que no sea entero
+
+*/
