f9f2c048688bdcac7c40006afdde26e1f227a459
LautaroHernanLopez <lautilo.0770@gmail.com>
2026-06-14 18:51:29 -0300

    Update solucion.scala

--- comparativa/solucion.scala
+++ comparativa/solucion.scala
@@ -22,8 +22,6 @@ if (
 	}
 }
 
-def transicion(coloractual: String, cambiara: String): List[String] = { if (((coloractual == "en-rojo") && (cambiara == "verde")) || ((coloractual == "en-verde") && (cambiara == "amarillo")) || ((coloractual == "en-amarillo") && (cambiara == "rojo"))){List(coloractual, "cambiar-a-".+(cambiara))} else {List (coloractual, "accion-por-defecto")}}
-
 /*
 CASOS DE PRUEBA
 Comportamiento Normal:
@@ -34,6 +32,49 @@ Caso de Error:
 	transicion(8, "rojo") -> error: type mismatch;
         					found   : Int(8)
         					required: String 
+*/
+
+/*
+FUNCION: transicion_inter
+NATURALEZA: pura
+ESTRATEGIA: condicional, construccion de lista  
+IMPACTO: no destructiva
+*/
+
+def transicion_inter(coloractual: String, cambiara: String): List[String] = {
+if (
+	((coloractual == "en-rojo") && (cambiara == "rojo-intermitente")) ||
+	((coloractual == "en-verde") && (cambiara == "verde-intermitente")) ||
+	((coloractual == "en-amarillo") && (cambiara == "amarillo-intermitente")) ||
+	((coloractual == "en-rojo-intermitente") && (cambiara == "verde")) ||
+	((coloractual == "en-verde-intermitente") && (cambiara == "amarillo")) ||
+	((coloractual == "en-amarillo-intermitente") && (cambiara == "rojo")) 
+	) 
+{
+
+	List(coloractual, "cambiar-a-".+(cambiara))
+
+} else {
+
+	List (coloractual, "accion-por-defecto")
+
+	}
+}
+
+
+/*
+CASOS DE PRUEBA
+Comportamiento Normal:
+	transicion_inter("en-rojo", "rojo-intermitente") -> List(en-rojo, cambiar-a-rojo-intermitente)
+	transicion_inter("en-verde", "verde-intermitente") -> List(en-verde, cambiar-a-verde-intermitente)
+	transicion_inter("en-amarillo", "amarillo-intermitente") ->  List(en-amarillo, cambiar-a-amarillo-intermitente)
+	transicion_inter("en-rojo-intermitente", "verde") -> List(en-rojo-intermitente, cambiar-a-verde)
+	transicion_inter("en-verde-intermitente", "amarillo") -> List(en-verde-intermitente, cambiar-a-amarillo)
+	transicion_inter("en-amarillo-intermitente", "rojo") -> List(en-amarillo-intermitente, cambiar-a-rojo)
+Caso de Error:
+	transicion_inter(8, "rojo") -> error: type mismatch;
+        					found   : Int(8)
+        					required: String
 */
 
 /*
