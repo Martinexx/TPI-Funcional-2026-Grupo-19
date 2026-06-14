f3e3c95b427900e41759c053e03780e95b75990c
LautaroHernanLopez <lautilo.0770@gmail.com>
2026-06-14 18:45:40 -0300

    Update solucion.scala

--- comparativa/solucion.scala
+++ comparativa/solucion.scala
@@ -1,4 +1,42 @@
 /*
+FUNCION: transicion
+NATURALEZA: pura
+ESTRATEGIA: condicional, construccion de lista  
+IMPACTO: no destructiva
+*/
+
+def transicion(coloractual: String, cambiara: String): List[String] = {
+if (
+	((coloractual == "en-rojo") && (cambiara == "verde")) ||
+	((coloractual == "en-verde") && (cambiara == "amarillo")) ||
+	((coloractual == "en-amarillo") && (cambiara == "rojo"))
+	) 
+{
+
+	List(coloractual, "cambiar-a-".+(cambiara))
+
+} else {
+
+	List (coloractual, "accion-por-defecto")
+
+	}
+}
+
+def transicion(coloractual: String, cambiara: String): List[String] = { if (((coloractual == "en-rojo") && (cambiara == "verde")) || ((coloractual == "en-verde") && (cambiara == "amarillo")) || ((coloractual == "en-amarillo") && (cambiara == "rojo"))){List(coloractual, "cambiar-a-".+(cambiara))} else {List (coloractual, "accion-por-defecto")}}
+
+/*
+CASOS DE PRUEBA
+Comportamiento Normal:
+	transicion("en-rojo", "verde") -> List[String] = List(en-rojo, cambiar-a-verde)
+	transicion("en-verde", "amarillo") -> List[String] = List(en-verde, cambiar-a-amarillo)
+	transicion("en-amarillo", "rojo") ->  List[String] = List(en-amarillo, cambiar-a-rojo)
+Caso de Error:
+	transicion(8, "rojo") -> error: type mismatch;
+        					found   : Int(8)
+        					required: String 
+*/
+
+/*
 Funcion: timer
 Naturaleza: pura
 Estrategia: utilizacion de condicionales para determinar el rango
