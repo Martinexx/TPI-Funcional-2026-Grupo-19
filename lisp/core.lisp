d205ca1355cbbc2a7b503c6e0e588dabe141d5e3
LautaroHernanLopez <lautilo.0770@gmail.com>
2026-06-20 10:25:54 -0300

    Update core.lisp
    transicion con intermitencia

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,16 +1,20 @@
 #|
-FUNCION: transicion
+FUNCION: Transicion
 NATURALEZA: Pura
-ESTRATEGIA: Construcion de lista + condicional
-IMPACTO: no destructiva
+ESTRATEGIA: Construccion de lista + condicional
+IMPACTO: No destructiva
 |#
 
 (defun transicion (color-actual cambiar-a)
 	(list color-actual 
 		(cond 
-			((AND (equalp color-actual 'en-amarillo) (equalp cambiar-a 'rojo)) "cambiar-a-rojo")
-			((AND (equalp color-actual 'en-rojo) (equalp cambiar-a 'verde)) "cambiar-a-verde")
-			((AND (equalp color-actual 'en-verde) (equalp cambiar-a 'amarillo)) "cambiar-a-amarillo")
+			((AND (equalp color-actual 'en-amarillo-intermitente) (equalp cambiar-a 'rojo)) "cambiar-a-rojo")
+			((AND (equalp color-actual 'en-rojo-intermitente) (equalp cambiar-a 'verde)) "cambiar-a-verde")
+			((AND (equalp color-actual 'en-verde-intermitente) (equalp cambiar-a 'amarillo)) "cambiar-a-amarillo")
+
+			((AND (equalp color-actual 'en-rojo) (equalp cambiar-a 'rojo-intermitente)) "cambiar-a-rojo-intermitente")
+			((AND (equalp color-actual 'en-verde) (equalp cambiar-a 'verde-intermitente)) "cambiar-a-verde-intermitente")
+			((AND (equalp color-actual 'en-amarillo) (equalp cambiar-a 'amarillo-intermitente)) "cambiar-a-amarillo-intermitente")
 			(t 'accion-por-defecto)
 		)
 	)
@@ -19,14 +23,13 @@ IMPACTO: no destructiva
 #|
 CASOS DE PRUEBA
 Comportamiento Normal:
-	(transicion 'en-amarillo 'rojo) -> (EN-AMARILLO "cambiar-a-rojo")
-	(transicion 'en-rojo 'verde) -> (EN-ROJO "cambiar-a-verde")
-	(transicion 'en-verde 'amarillo) -> (EN-VERDE "cambiar-a-amarillo")
+	(transicion 'en-amarillo-intermitente 'rojo) -> (EN-AMARILLO-INTERMITENTE "cambiar-a-rojo")
+	(transicion 'en-verde 'verde-intermitente) -> (EN-VERDE "cambiar-a-verde-intermitente")
 
-Comportamiento Alternativo:
-	(transicion 'en-rojo 'rojo) -> (EN-ROJO ACCION-POR-DEFECTO)
-	(transicion 'texto-sin 'sentido) -> (TEXTO-SIN ACCION-POR-DEFECTO)
-|#
+Comportamiento Alternativo:	
+	(transicion 'en-rojo-intermitente 'amarillo) -> (EN-ROJO-INTERMITENTE ACCION-POR-DEFECTO)
+	(transicion 'cualquier-cosa 8) -> (CUALQUIER-COSA ACCION-POR-DEFECTO)	
+|#	
 
 #|
 NOMBRE: timer
