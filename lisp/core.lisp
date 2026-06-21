e87b0a1e88199c3fda309b4f94604e1eb22a8473
valentino-jara-rey <valentinojararey@gmail.com>
2026-06-20 22:05:49 -0300

    update de estilo

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -6,19 +6,14 @@ IMPACTO: No destructiva
 |#
 
 (defun transicion (color-actual cambiar-a)
-	(list color-actual 
-		(cond 
-			((AND (equalp color-actual 'en-amarillo-intermitente) (equalp cambiar-a 'rojo)) "cambiar-a-rojo")
-			((AND (equalp color-actual 'en-rojo-intermitente) (equalp cambiar-a 'verde)) "cambiar-a-verde")
-			((AND (equalp color-actual 'en-verde-intermitente) (equalp cambiar-a 'amarillo)) "cambiar-a-amarillo")
-
-			((AND (equalp color-actual 'en-rojo) (equalp cambiar-a 'rojo-intermitente)) "cambiar-a-rojo-intermitente")
-			((AND (equalp color-actual 'en-verde) (equalp cambiar-a 'verde-intermitente)) "cambiar-a-verde-intermitente")
-			((AND (equalp color-actual 'en-amarillo) (equalp cambiar-a 'amarillo-intermitente)) "cambiar-a-amarillo-intermitente")
-			(t 'accion-por-defecto)
-		)
-	)
-)
+ (list color-actual 
+  (cond  ((AND (equalp color-actual 'en-amarillo-intermitente) (equalp cambiar-a 'rojo)) "cambiar-a-rojo")
+		 ((AND (equalp color-actual 'en-rojo-intermitente) (equalp cambiar-a 'verde)) "cambiar-a-verde")
+	     ((AND (equalp color-actual 'en-verde-intermitente) (equalp cambiar-a 'amarillo)) "cambiar-a-amarillo")
+		 ((AND (equalp color-actual 'en-rojo) (equalp cambiar-a 'rojo-intermitente)) "cambiar-a-rojo-intermitente")
+		 ((AND (equalp color-actual 'en-verde) (equalp cambiar-a 'verde-intermitente)) "cambiar-a-verde-intermitente")
+		 ((AND (equalp color-actual 'en-amarillo) (equalp cambiar-a 'amarillo-intermitente)) "cambiar-a-amarillo-intermitente")
+		 (t 'accion-por-defecto))))
 
 #|
 CASOS DE PRUEBA
@@ -39,15 +34,12 @@ IMPACTO: no destructiva
 |#
 
 (defun timer (tiempo-actual)
-	(COND 
-		((<= (MOD tiempo-actual (duracion-ciclo)) 89) 'rojo)
+(COND   ((<= (MOD tiempo-actual (duracion-ciclo)) 89) 'rojo)
 		((<= (MOD tiempo-actual (duracion-ciclo)) 92) 'rojo-intermitente)
 		((<= (MOD tiempo-actual (duracion-ciclo)) 212) 'verde)
 		((<= (MOD tiempo-actual (duracion-ciclo)) 215) 'verde-intermitente)
 		((<= (MOD tiempo-actual (duracion-ciclo)) 221) 'amarillo)
-		(t 'amarillo-intermitente)
-	)
-)
+		(t 'amarillo-intermitente)))
 
 #|
 CASOS DE PRUEBA
@@ -71,7 +63,7 @@ Caso de Error:
  	=================================== |#
 
 (defun registrar-auditoria (color-anterior color-nuevo)
-  (format t "Tiempo ~A: la luz ha cambiado de ~A a ~A~%"  
+(format t "Tiempo ~A: la luz ha cambiado de ~A a ~A~%"  
 (local-time:format-timestring nil (local-time:now):format '((:year 4) "-" (:month 2) "-" (:day 2) " " (:hour 2) ":" (:min 2)))
 (CAR (transicion color-anterior color-nuevo))  (CADR (transicion color-anterior color-nuevo ))))
 
@@ -91,17 +83,10 @@ IMPACTO: No destructivo
 |#
 
 (defun informe (color-anterior color-nuevo)
-(with-open-file (stream
-                 "informe-ejecucion-semaforo.txt"
-                 :direction :output
-                 :if-exists :append)
-  
+(with-open-file (stream "informe-ejecucion-semaforo.txt" :direction :output :if-exists :append)
 (format stream "Informe de Ejecución del Sistema Semafórico~%")
 (format stream "=========================================~%")
-
 (format stream (registrar-auditoria color-anterior color-nuevo))
-
-
 (format stream "~% --- Fin del Informe ---")))
 
 #|
@@ -119,8 +104,7 @@ Impacto: no destructiva
 |#
 
 (defun duracion-ciclo ()
-	(+ 90 3 120 3 6 3)
-)
+	(+ 90 3 120 3 6 3))
 
 #|
 CASO DE PRUEBA
@@ -135,12 +119,9 @@ IMPACTO: no destructiva
 |#
 
 (defun recomendacion-ciclo (duracion-ciclo)
-	(cond  
-		((< duracion-ciclo 35) "llegue al minimo de 35")
+(cond  ((< duracion-ciclo 35) "llegue al minimo de 35")
 		((> duracion-ciclo 150) "baje hasta 150")
-		(t "esta en rango optimo")
-	)
-)
+		(t "esta en rango optimo")))
 
 #|
 CASOS DE PRUEBA
@@ -161,8 +142,7 @@ IMPACTO: No destructiva
 |#
 
 (defun ciclos-por-tiempo (minutos) 
-	(values (TRUNCATE (/ (* minutos 60) (duracion-ciclo))))
-)
+  (values (TRUNCATE (/ (* minutos 60) (duracion-ciclo)))))
 
 ;Si se le pasa solo un número a truncate este devuelve la parte entera y la parte decimal del número por separado, 
 ;devuelve 2 valores, pero al usarse como entrada de otra funcion, esta tomara solo el primer valor devuelto,
@@ -186,16 +166,13 @@ IMPACTO: no destructiva
 |#
 
 (defun proximo-color (horaUnix)
-	(+ (- horaUnix (MOD horaUnix (duracion-ciclo))) (cond 
-														((equal (timer horaUnix) 'rojo) 93)
-														((equal (timer horaUnix) 'rojo-intermitente) 93)
-														((equal (timer horaUnix) 'verde) 216)
-														((equal (timer horaUnix) 'verde-intermitente) 216)
-														((equal (timer horaUnix) 'amarillo) 225)
-														((equal (timer horaUnix) 'amarillo-intermitente) 225)
-													) 
-	)
-)
+  (+ (- horaUnix (MOD horaUnix (duracion-ciclo))) 
+	(cond ((equal (timer horaUnix) 'rojo) 93)
+		  ((equal (timer horaUnix) 'rojo-intermitente) 93)
+          ((equal (timer horaUnix) 'verde) 216)
+		  ((equal (timer horaUnix) 'verde-intermitente) 216)
+		  ((equal (timer horaUnix) 'amarillo) 225)
+		   ((equal (timer horaUnix) 'amarillo-intermitente) 225))))
 
 #|
 CASOS DE PRUEBA
@@ -218,16 +195,12 @@ IMPACTO: no destructiva
 |#
 
 (defun color-complementario (color)
-	(COND
-		((equal color 'rojo) 'rojo-intermitente)
+  (COND ((equal color 'rojo) 'rojo-intermitente)
 		((equal color 'verde) 'verde-intermitente)
 		((equal color 'amarillo) 'amarillo-intermitente)
-
 		((equal color 'rojo-intermitente) 'rojo)
 		((equal color 'verde-intermitente) 'verde)
-		((equal color 'amarillo-intermitente) 'amarillo)
-	)
-)
+		((equal color 'amarillo-intermitente) 'amarillo)))
 
 #|
 CASOS DE PRUEBA
@@ -250,15 +223,10 @@ IMPACTO: no destructiva
 |#
 
 (defun veces_periodo (ini fin color)
-	(COND 
-		((> ini fin) 0)
-		(
-			(OR (equal (timer ini) color) (equal (timer ini) (color-complementario color)))
-			(+ 1 (veces_periodo (proximo-color ini) fin color))
-		)
-		(t (veces_periodo (proximo-color ini) fin color))
-	)
-)
+	(COND ((> ini fin) 0)
+		((OR (equal (timer ini) color) (equal (timer ini) (color-complementario color))) 
+		(+ 1 (veces_periodo (proximo-color ini) fin color)))
+		(t (veces_periodo (proximo-color ini) fin color))))
 
 #|
 CASOS DE PRUEBA
